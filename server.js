6ca77a9da4d1622220986d4975fcf51411cf0a6b
Felipe Mendes <lfelipecmendes@icloud.com>
2025-03-14 12:19:57 -0300

    feat(server): allow write operations on db

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+
+// Uncomment to allow write operations
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+// const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"/api/*": "/$1",
-	})
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
 );
 server.use(router);
 server.listen(3000, () => {
-	console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
